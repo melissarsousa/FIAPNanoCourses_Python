244cdce7505790df2b08328461a8654885354403
melissarsousa <melissa.rios.sousa@gmail.com>
2023-01-27 15:32:14 -0300

    Minor changes in code

--- Localizacao_Python/Enderecos.py
+++ Localizacao_Python/Enderecos.py
@@ -1,7 +1,7 @@
 from pygeocoder import Geocoder
 
 endereco = 'avenida paulista, 100 Sao Paulo'
-print(Geocoder('AIzaSyANRBN8b4VZm6K1qdRpsruGji4FnKBjyj4').geocode(endereco))
-print(Geocoder('AIzaSyANRBN8b4VZm6K1qdRpsruGji4FnKBjyj4').geocode(endereco).postal_code)
-print(Geocoder('AIzaSyANRBN8b4VZm6K1qdRpsruGji4FnKBjyj4').geocode(endereco).coordinates)
-print(Geocoder('AIzaSyANRBN8b4VZm6K1qdRpsruGji4FnKBjyj4').reverse_geocode(-23.5709484, -46.644044))
+print(Geocoder('GoogleCloudAPIKey').geocode(endereco))
+print(Geocoder('GoogleCloudAPIKey').geocode(endereco).postal_code)
+print(Geocoder('GoogleCloudAPIKey').geocode(endereco).coordinates)
+print(Geocoder('GoogleCloudAPIKey').reverse_geocode(-23.5709484, -46.644044))
--- Localizacao_Python/Primeiros_Passos.py
+++ Localizacao_Python/Primeiros_Passos.py
@@ -1,4 +1,4 @@
 from pygeocoder import Geocoder
 
 endereco = '1222, Lins de Vasconcelos, Sao Paulo, SP'
-print(Geocoder('AIzaSyANRBN8b4VZm6K1qdRpsruGji4FnKBjyj4').geocode(endereco).coordinates)
+print(Geocoder('GoogleCloudAPIKey').geocode(endereco).coordinates)
